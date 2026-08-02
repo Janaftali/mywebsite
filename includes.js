084255beeddf23a4f826f3eb8efb6db24802e194
Janaftali <jnfeith@outlook.com>
2026-08-02 18:24:34 +0000

    Add files via upload

--- includes.js
+++ includes.js
@@ -81,3 +81,9 @@ function closeProject(){
   var projectFrame = document.getElementById("projectFrame");
   projectFrame.style.display="none";
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
